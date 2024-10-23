caae68759affdf15c2f98ad30d4a2716e2637327
gopa-noaa <gopa.padmanabhan@.noaa.gov>
2024-10-23 11:46:53 -0600

    exit on null doc

--- statToCbDoc/statToCbDoc.go
+++ statToCbDoc/statToCbDoc.go
@@ -364,8 +364,8 @@ func main() {
 		}
 	*/
 
-	log.Printf("\tstatToCbDoc, files:%d, file-counts:[%d,%d], db-counts[%d,%d] finished in %v", len(inputFiles),
-		fileTotalCount, fileTotalErrors, dbTotalCount, dbTotalErrors, time.Since(start))
+	log.Printf("\tstatToCbDoc, files:%d, docs:%d, file-stats:[%d,%d], db-stats[%d,%d] finished in %v", len(inputFiles),
+		fileTotalCount, len(cbDocs), fileTotalErrors, dbTotalCount, dbTotalErrors, time.Since(start))
 }
 
 func parseLoadSpec(file string) (LoadSpec, error) {
